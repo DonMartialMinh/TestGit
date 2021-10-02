dc1a928f8c96e8dc2f90e36c392fcc1bc883de59
Vo Minh Don <vominhdona1@gmail.com>
2021-10-02 09:06:56 +0700

    feat: add another story

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is a print")
+        print("Add story two")
     }
 
 
