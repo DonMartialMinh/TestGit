2063b55bbb52df29177a57fb897ab7f2e2374fec
Vo Minh Don <vominhdona1@gmail.com>
2021-10-04 08:45:13 +0700

    add story four

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is a print")
+        print("Added story four")
     }
-
-
 }
 
